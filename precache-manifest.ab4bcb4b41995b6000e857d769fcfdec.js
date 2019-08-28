16f1b523abef4c3826bd474a2064c68a7b309552
Julio Cesar Ceron <julio.ceorn@gmail.com>
2019-08-27 23:24:26 -0300

    Updates

---
+++ precache-manifest.ab4bcb4b41995b6000e857d769fcfdec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5556092102a7c2a37c23a6213b63c2f",
+    "url": "/Minesweeper/index.html"
+  },
+  {
+    "revision": "ba4b8270a146d68cf5d5",
+    "url": "/Minesweeper/static/css/main.2e06865b.chunk.css"
+  },
+  {
+    "revision": "621f5b448ccdf5bd4cd3",
+    "url": "/Minesweeper/static/js/2.8c341ced.chunk.js"
+  },
+  {
+    "revision": "ba4b8270a146d68cf5d5",
+    "url": "/Minesweeper/static/js/main.cfc0d12c.chunk.js"
+  },
+  {
+    "revision": "fb47186c0c997709cc69",
+    "url": "/Minesweeper/static/js/runtime~main.4f5ea6f9.js"
+  },
+  {
+    "revision": "a4ec3fa6a6bb95966007f5bf169b28ae",
+    "url": "/Minesweeper/static/media/bomb-explosion.a4ec3fa6.svg"
+  }
+]);
