a1494ba11c8c73c2b36a5d666e45f06d413b75a4
julio.ceron <julio.ceorn@gmail.com>
2020-05-27 23:32:57 -0300

    refactor App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
-
 import Minesweeper from "./components/Minesweeper/Minesweeper";
 
-
-function App() {
-	return <Minesweeper />
-}
+const App = () => <Minesweeper />;
 
 export default App;
